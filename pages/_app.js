c80810abee54a96a8f5be236c57459600fdb1bbe
rdupo <67809219+rdupo@users.noreply.github.com>
2024-02-25 17:21:48 -0600

    update favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 import { WalletProvider } from '../contexts/WalletContext';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <WalletProvider>
+      <Head>
+        <link rel="icon" href="/vphree24.png" />
+      </Head>
       <Component {...pageProps} />
     </WalletProvider>
   );
