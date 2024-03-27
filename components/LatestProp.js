19391e822fa550f4ddb3986619ec77f4b153384c
rdupo <67809219+rdupo@users.noreply.github.com>
2024-03-27 09:11:07 -0500

    Update LatestProp.js
    addressing issue w polls.results[0] being undefined

--- components/LatestProp.js
+++ components/LatestProp.js
@@ -29,7 +29,7 @@ function LatestProp() {
         <div className="w-10/12">
           <p className="v3-txt">Poll #{polls.id}</p>
           <p className="collection-desc text-gray-300">{polls.description}</p>
-          {polls.until > Date.now() && polls.results ?
+          {polls.until > Date.now() && typeof(polls.results[0]) !== 'undefined' ?
             <p className="collection-desc text-gray-300">Ends: {polls.until}</p>
             :
             <div>
