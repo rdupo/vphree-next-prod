14de0b4e9f5335aa9bf9b00e138c747f0db94e08
rdupo <67809219+rdupo@users.noreply.github.com>
2024-05-17 07:03:22 -0500

    fix bg color on DOPE btn

--- pages/index.js
+++ pages/index.js
@@ -161,7 +161,7 @@ export default function Home() {
               <button className="w-60 mobile-100 cta g-b g-txt black-bg">v3 Marketplace</button>
             </Link>
             <Link href="/dose-of-phunks">
-              <button className="w-60 mobile-100 cta bg-[##ffb900] black-txt b-b">Dose of Phunks</button>
+              <button className="w-60 mobile-100 cta bg-[#ffb900] black-txt b-b">Dose of Phunks</button>
             </Link>
           </div>
         </div>
